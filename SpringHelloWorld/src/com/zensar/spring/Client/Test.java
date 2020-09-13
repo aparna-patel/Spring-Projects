ef187b4364725fd7cdc4311a069d371d19056db2
Aparna <aparnapatel1234@outlook.com>
2020-09-13 20:25:09 +0530

    spring core

---
+++ SpringHelloWorld/src/com/zensar/spring/Client/Test.java
@@ -0,0 +1,16 @@
+package com.zensar.spring.Client;
+
+import com.zensar.spring.beans.Employee;
+
+public class Test {
+	
+	public  static void main(String[] args)
+	{
+		
+		ApplicationContext  context;
+		
+		//Employee employee = new Employee();
+		//System.out.println(employee);
+	}
+
+}
